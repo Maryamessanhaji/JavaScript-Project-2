01ec1894cde14b3d376051d1b6e4a7e7d183d084
Maryamessanhaji <essanhajimaryam10@gmail.com>
2025-04-18 11:51:04 +0100

    create function countCharacters

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// String Manipulation Functions:
 function reversesString(str) {
     let reverses = '';
     for (let index = str.length - 1; index >= 0; index--) {
@@ -6,3 +7,19 @@ function reversesString(str) {
     }
 }
 
+function countCharacters(str) {
+    let len;
+    len = str.length
+   return len ;
+    
+}
+// function capitalizeWords(word){
+//     let firstWord = word.splite(1,0)
+    
+//         console.log(firstWord);
+
+    
+// console.log(word.toLowerCase());
+// console.log(word.toUpperCase());
+
+// }
