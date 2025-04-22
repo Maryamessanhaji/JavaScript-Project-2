2f3bedea639a545ab99e620e13f4a9a07f3b70f4
Maryamessanhaji <essanhajimaryam10@gmail.com>
2025-04-22 09:56:27 +0100

    add functions sum and minMax and CapitalizeWords

--- script.js
+++ script.js
@@ -13,13 +13,51 @@ function countCharacters(str) {
    return len ;
     
 }
-// function capitalizeWords(word){
-//     let firstWord = word.splite(1,0)
-    
-//         console.log(firstWord);
+function capitalizeWords(sentence) {
+    let result = "";
+    let capitalizeNext = true;
+  
+    for (let i = 0; i < sentence.length; i++) {
+      let char = sentence[i];
+  
+      if (capitalizeNext && char >= 'a' && char <= 'z') {
+        // Convert lowercase letter to uppercase
+        char = String.fromCharCode(char.charCodeAt(0) - 32);
+        capitalizeNext = false;
+      } else if (char === " ") {
+        capitalizeNext = true;
+      } else {
+        capitalizeNext = false;
+      }
+  
+      result += char;
+    }
+  
+    return result;
+  }
 
-    
-// console.log(word.toLowerCase());
-// console.log(word.toUpperCase());
+  
+// Array Functions:
+function findMaxMin(arr) {
+    let max = arr[0]; 
+    let min = arr[0]; 
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+    return [max,min];
+  }
 
-// }
+  function Sum(arr){
+    let sum =0
+    for (let i = 0; i < arr.length; i++) {
+        sum+=  arr[i]
+       
+    }
+    return sum
+  }
+  
+  
