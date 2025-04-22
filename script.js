e80ab3f326ac271df09bab5abeb71c4e25c1e697
Maryamessanhaji <essanhajimaryam10@gmail.com>
2025-04-22 10:05:20 +0100

    is prime

--- script.js
+++ script.js
@@ -78,4 +78,16 @@ function factorial(n) {
     }
     return result;
   }
+  function isPrime(n) {
+    if (n <= 1){
+        return false; // 0 and 1 are not prime
+    } 
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false; 
+      }
+    }
+    return true; 
+  }
+  
   
