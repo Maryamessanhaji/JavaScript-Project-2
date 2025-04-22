a8b603227513ccf840962b4c73cd6a997b467d88
Maryamessanhaji <essanhajimaryam10@gmail.com>
2025-04-22 09:59:54 +0100

    add function filterArray

--- script.js
+++ script.js
@@ -60,4 +60,14 @@ function findMaxMin(arr) {
     return sum
   }
   
+  function filterArray(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+ 
   
